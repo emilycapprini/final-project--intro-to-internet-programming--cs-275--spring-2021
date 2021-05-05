53f19ca386e4f43adf67f150bbaa0db21e2806ea
Emily Capprini <ecapprini@gmail.com>
2021-05-04 22:39:48 -0400

    Get input for diamond size

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,12 @@
-window.onload = () => {
-    console.log(`%c JavaScript works!`,
-        `background-color: yellow; font-weight: bold;`);
+window.onload  = () => {
+    // Set intial input value to a string
+    let input = `nothing`;
+
+    // While input is not a number and too small keep prompting for new input
+    while(isNaN(input.valueOf()) || input.valueOf() < 2){
+        input = window.prompt(`Enter the size of your diamond as a number:`);
+    }
+    // Rounding up the input to an integer
+    input = Math.ceil(parseFloat(input));
+
 };
