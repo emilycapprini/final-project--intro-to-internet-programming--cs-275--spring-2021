66ec4c60f6b8ca63999bb2f9d32e50704f1eb7c1
Emily Capprini <ecapprini@gmail.com>
2021-05-05 00:05:04 -0400

    Creating the diamond in the div

--- app/js/app.js
+++ app/js/app.js
@@ -9,4 +9,50 @@ window.onload  = () => {
     // Rounding up the input to an integer
     input = Math.ceil(parseFloat(input));
 
+    // Getting div element
+    let diamond = document.getElementById(`diamond`);
+
+    // Making the diamond
+    // variable to maintain the i value for outside the for loop
+    let holder = 0;
+    // create the upper half of the diamond
+    for(let i = 1; i <= input; i+= 2){
+        // add spaces to center the row
+        for(let j = input-1; j >= i; j--){
+            diamond.innerHTML += ` `;
+        }
+        // add appropraite number of stars
+        for(let k = 1; k<= i; k++){
+            diamond.innerHTML += `* `;
+        }
+        // add a new line after each row of stars
+        diamond.innerHTML += `<br>`;
+        // update the holder variable
+        holder = i + 1;
+        // if input is even set i = 0 so it prints only an even amount of stars
+        if(input % 2 === 0 && i === 1){
+        i = 0;
+        }
+    }
+    // if it has printed the input number of stars
+    if (holder === input+1){
+        // create the lower half of the diamond
+        for(let i = 1; i <= input-2; i+=2){
+            // add spaces to center the row
+            for(let k = 1; k<= i; k++){
+                diamond.innerHTML += ` `;
+            }
+            // add appropriate number of stars
+            for(let j = i; j <= input-2; j++){
+                diamond.innerHTML += `* `;
+            }
+            // add a new line after each row of stars
+            diamond.innerHTML += `<br>`;
+        }
+        // if the input is zero add and extra star for the bottom row
+        if(input % 2 === 0) {
+            // add the last star
+            diamond.innerHTML += `*`;
+        }
+    }
 };
